b415f0307788a50d057d2354e1c8afd0994a1772
Atik <atik.wowspace@gmail.com>
2024-02-12 22:29:44 +0600

    Added soltion for python

---
+++ problem-1045/python/sol1045.py
@@ -0,0 +1,22 @@
+# Read 3 doubles (sides of triangle) covert them into a list
+sides = [float(i) for i in input().split()]
+sides.sort(reverse=True)  # sort them in descending order
+A, B, C = sides  # unpack the sides into three variables
+
+if A >= B + C:
+    print("NAO FORMA TRIANGULO")
+
+if A**2 == B**2 + C**2:
+    print("TRIANGULO RETANGULO")
+
+if A**2 > B**2 + C**2:
+    print("TRIANGULO OBTUSANGULO")
+
+if A**2 < B**2 + C**2:
+    print("TRIANGULO ACUTANGULO")
+
+if A == B == C:
+    print("TRIANGULO EQUILATERO")
+
+if A == B != C or A != B == C or B != C == A:
+    print("TRIANGULO ISOSCELES")
