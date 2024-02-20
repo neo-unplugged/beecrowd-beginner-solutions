6beb7321c987ea022a0d839ab861ac3c88757278
Atik <atik.wowspace@gmail.com>
2024-02-20 19:32:24 +0600

    Added soltion for python 1021 - bank notes and coins

--- problem-1021/BEE1021.py
+++ problem-1021/BEE1021.py
@@ -3,6 +3,7 @@ N = float(input())
 
 units = [10000, 5000, 2000, 1000, 500, 200, 100, 50, 25, 10, 5, 1]
 credit = int(N * 100)  # turn the credit into cents and trim the changes
+title = "nota(s)"
 
 print("NOTAS:")
 for i in units:
@@ -10,4 +11,5 @@ for i in units:
     credit %= i
     if units.index(i) == 6:
         print("MOEDAS:")
-    print("{} nota(s) de R$ {:.2f}".format(count, i/100))
+        title = "moeda(s)"
+    print("{} {} de R$ {:.2f}".format(count, title, i/100))
