abf3f55c8686a6b202b0f752bc7f3a919c28327a
Atik <atik.wowspace@gmail.com>
2024-02-20 19:18:04 +0600

    Added soltion for python 1021 - bank notes and coins

--- problem-1021/BEE1021.py
+++ problem-1021/BEE1021.py
@@ -1,2 +1,13 @@
 # credit user given to bank
 N = float(input())
+
+units = [10000, 5000, 2000, 1000, 500, 200, 100, 50, 25, 10, 5, 1]
+credit = int(N * 100)  # turn the credit into cents and trim the changes
+
+print("NOTAS:")
+for i in units:
+    count = credit // i
+    credit %= i
+    if units.index(i) == 6:
+        print("MOEDAS:")
+    print("{} nota(s) de R$ {:.2f}".format(count, i/100))
