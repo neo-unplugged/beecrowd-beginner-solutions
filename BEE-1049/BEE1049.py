bce87f50e2ccb51bc110af35b9932ca3f689b8e2
Atik <atik.wowspace@gmail.com>
2024-02-27 04:04:05 +0600

    python solution for BEE-1049 and BEE-1074

---
+++ BEE-1049/BEE1049.py
@@ -0,0 +1,29 @@
+# animals
+a = input()
+b = input()
+c = input()
+
+animals = {
+    'vertebrado': {
+        'ave': {
+            'carnivoro': 'aguia',
+            'onivoro': 'pomba'
+        },
+        'mamifero': {
+            'onivoro': 'homem',
+            'herbivoro': 'vaca'
+        }
+    },
+    'invertebrado': {
+        'inseto': {
+            'hematofago': 'pulga',
+            'herbivoro': 'lagarta'
+        },
+        'anelideo': {
+            'hematofago': 'sanguessuga',
+            'onivoro': 'minhoca'
+        }
+    }
+}
+
+print(animals[a][b][c])
